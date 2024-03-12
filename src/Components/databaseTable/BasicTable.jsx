f9986beb1f5ac4dfdf081818bc9960b39a8049c8
Night-Fares <fares.lassoued@etudiant-isi.utm.tn>
2024-03-12 16:17:12 +0100

    update table component

--- src/Components/databaseTable/BasicTable.jsx
+++ src/Components/databaseTable/BasicTable.jsx
@@ -1,6 +1,11 @@
-import React, { useMemo, useState } from "react"
+import React, { useMemo, useState, useEffect } from "react"
+import { handleExport } from "./handleFile"
+import * as XLSX from "xlsx"
+import FileSaver from "file-saver"
+import SearchBar from "../SearchBar"
 import { FaSortUp, FaSortDown } from "react-icons/fa6"
 import "./table.css"
+import { FaXmark } from "react-icons/fa6"
 import {
   flexRender,
   getCoreRowModel,
@@ -10,12 +15,60 @@ import {
   getFilteredRowModel,
 } from "@tanstack/react-table"
 import { columnDef } from "./columns"
-const BasicTable = ({ dataJson, filtering, setFiltering }) => {
+const FILE_TYPE =
+  "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8"
+const FILE_EXTENTION = ".xlsx"
+const BasicTable = () => {
+  const [filtering, setFiltering] = useState("")
+  const [data, setData] = useState([])
+  const [rows, setRows] = useState(0)
+  const [error, setError] = useState(false)
+  const [jsx, setJsx] = useState(<div></div>)
+  const [active, setActive] = useState(true)
+  const handleFileUpload = (e) => {
+    const reader = new FileReader()
+    reader.readAsBinaryString(e.target.files[0])
+    reader.onload = (e) => {
+      const data = e.target.result
+      const workbook = XLSX.read(data, { type: "binary" })
+      const sheetName = workbook.SheetNames[0]
+      const sheet = workbook.Sheets[sheetName]
+      const parseData = XLSX.utils.sheet_to_json(sheet)
+      setData(parseData)
+      setRows(parseData.length)
+      console.log(Object.keys(parseData[0]))
+    }
+  }
+  useEffect(() => {
+    if (error)
+      setJsx(
+        <div className="database-error-message">
+          <p>Alert! there is an error</p>
+        </div>
+      )
+    else if (rows !== 0)
+      setJsx(
+        <div
+          className={
+            active
+              ? "database-success-message"
+              : "database-success-message hide"
+          }
+        >
+          <p>You found {rows} results</p>
+          <FaXmark
+            className="message-icon"
+            onClick={() => setActive(!active)}
+          />
+        </div>
+      )
+  }, [data, active])
   const [sorting, setSorting] = useState([])
   const finalColumn = useMemo(() => columnDef, [])
+  const finalData = useMemo(() => data, [data])
   const tableInstance = useReactTable({
     columns: finalColumn,
-    data: dataJson,
+    data: finalData,
     getCoreRowModel: getCoreRowModel(),
     getPaginationRowModel: getPaginationRowModel(),
     getSortedRowModel: getSortedRowModel(),
@@ -30,6 +83,29 @@ const BasicTable = ({ dataJson, filtering, setFiltering }) => {
   //console.log(tableInstance.getHeaderGroups())
   return (
     <>
+      <div className="database-upsection">
+        {jsx}
+        <div className="btn-group">
+          <SearchBar filtering={filtering} setFiltering={setFiltering} />
+          <label className="database-btn">
+            <input
+              type="file"
+              accept=".xlsx,.xls,.csv"
+              onChange={(e) => handleFileUpload(e)}
+              className="database-input"
+            />
+            Import
+          </label>
+          <button
+            className="database-btn"
+            onClick={() =>
+              handleExport(data, "data", FILE_TYPE, FILE_EXTENTION)
+            }
+          >
+            Export All
+          </button>
+        </div>
+      </div>
       <table>
         <thead>
           {tableInstance.getHeaderGroups().map((headerEl) => {
@@ -42,11 +118,12 @@ const BasicTable = ({ dataJson, filtering, setFiltering }) => {
                       colSpan={columnEl.colSpan}
                       onClick={columnEl.column.getToggleSortingHandler()}
                     >
-                      <div>
+                      <div className="table-sort">
                         {flexRender(
                           columnEl.column.columnDef.header,
                           columnEl.getContext()
                         )}
+
                         {
                           {
                             asc: <FaSortUp className="sorting-icon" />,
--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,10 @@
-import { configureStore } from "@reduxjs/toolkit";
-import loginSliceReducer from "./features/login/loginSlice";
-import registerSliceReducer from "./features/register/registerSlice";
+import { configureStore } from "@reduxjs/toolkit"
+import loginSliceReducer from "./features/login/loginSlice"
+import registerSliceReducer from "./features/register/registerSlice"
 
 export const store = configureStore({
   reducer: {
     loginSlice: loginSliceReducer,
     registerSlice: registerSliceReducer,
   },
-});
+})
